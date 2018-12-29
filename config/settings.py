74e546aa24bacfe4fb8bd16e960d6eb141921046
頼本圭太 <asbo0502@gmail.com>
2018-12-30 00:39:24 +0900

    scrapeApp first commit

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'scrape.apps.ScrapeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
